95c030678d73bfa7d731adbf1f061b2795cd64bb
TEE MS van der Linde <jattie.VanDerLinde@europe.tel.com>
2018-03-27 16:27:59 +0100

    create an iris.py scrip to import and use functions seperaately

---
+++ iris.py
@@ -0,0 +1,9 @@
+# Gerhard van der Linde - 27 March 2018
+# Student ID: G00364778
+# GMIT 52167 Final Project
+
+import functions as f
+
+data = f.read_csv_datafile(r'data/iris.data')
+f.creat_xy_plots(data)
+stat=f.calc_stats(data, screenprint=True)
